2d0645b4ea842da3b9ec4b85dcf6907ac5fa90e7
“AppDevIn” <teamprojectlive@gmail.com>
2020-05-06 20:30:05 +0800

    Added gridview, setup base adapter

--- app/src/main/java/com/mad/p03/np2020/routine/Adapter/HomePageAdapter.java
+++ app/src/main/java/com/mad/p03/np2020/routine/Adapter/HomePageAdapter.java
@@ -1,4 +1,31 @@
 package com.mad.p03.np2020.routine.Adapter;
 
-public class HomePageAdapter {
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+
+import java.util.ArrayList;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+class HomePageAdapter extends ArrayAdapter {
+
+    public HomePageAdapter(Context context, int textViewResourceId) {
+        super(context, textViewResourceId);
+    }
+
+
+    @Override
+    public int getCount() {
+        return super.getCount();
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        return super.getView(position, convertView, parent);
+    }
 }
--- app/src/main/java/com/mad/p03/np2020/routine/Home.java
+++ app/src/main/java/com/mad/p03/np2020/routine/Home.java
@@ -3,12 +3,39 @@ package com.mad.p03.np2020.routine;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.GridView;
 
 public class Home extends AppCompatActivity {
+
+    //Declare Constants
+
+
+    //Declare member variables
+    GridView mGridView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        //Find view by ID
+        mGridView = findViewById(R.id.section_grid_view);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        //TODO: Initialize any value
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    void hardCodeData(){
+        //TODO: Add in fake data
+
     }
 }
