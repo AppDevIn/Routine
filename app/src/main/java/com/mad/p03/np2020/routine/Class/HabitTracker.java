b5ae33fbfefcc6b3b69e0c2da4515a7e5c475fef
hwen <whman63@gmail.com>
2020-05-06 14:35:30 +0800

    properties for class Habit

--- app/src/main/java/com/mad/p03/np2020/routine/Class/HabitTracker.java
+++ app/src/main/java/com/mad/p03/np2020/routine/Class/HabitTracker.java
@@ -4,13 +4,40 @@ import java.util.List;
 
 class HabitTracker {
     
-    private String mTitle;
-    private String mDescription;
-    private String mDescNoun;
-    private String mOccurence;
-    private int mCount;
+    private String Title;
+    private int Occurence;
+    private int Count;
     private List<Integer> mCountList;
 
+    public String getTitle() {
+        return Title;
+    }
+
+    public void setTitle(String title) {
+        Title = title;
+    }
+
+    public int getOccurence() {
+        return Occurence;
+    }
+
+    public void setOccurence(int occurence) {
+        Occurence = occurence;
+    }
+
+    public int getCount() {
+        return Count;
+    }
+
+    public void setCount(int count) {
+        Count = count;
+    }
+
+    public HabitTracker(String title, int occurence, int count) {
+        Title = title;
+        Occurence = occurence;
+        Count = count;
+    }
 
     public HabitTracker() {
         // TODO: Please upload any chnages to this class to the main branch
