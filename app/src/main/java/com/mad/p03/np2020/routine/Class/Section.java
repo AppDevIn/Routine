a21cb323a7f8583beef92f03758645f4aaac11b4
“AppDevIn” <teamprojectlive@gmail.com>
2020-05-06 20:32:52 +0800

    Updated the section class with add and remove code

--- app/src/main/java/com/mad/p03/np2020/routine/Class/Section.java
+++ app/src/main/java/com/mad/p03/np2020/routine/Class/Section.java
@@ -7,15 +7,20 @@ class Section {
     private String mName;
     private List<Task> mTaskList;
 
-    public Section() {
+    public Section(String name,List<Task> taskList) {
+        this.mName = name;
+        this.mTaskList = taskList;
     }
 
     public void addTask(Task task){
         // TODO: Please upload any chnages to this class to the main branch`
+        mTaskList.add(task);
+
     }
     
-    public void rmTask(int UID){
+    public void rmTask(Task task){
         // TODO: Please upload any chnages to this class to the main branch`
+        mTaskList.remove(task);
     }
     
 }
