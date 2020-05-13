9dccec58e044bf8d7e791cd17aa67811e61882ba
iamgruuten <52743433+iamgruuten@users.noreply.github.com>
2020-05-13 10:09:19 +0800

    Added RecyclerView with custom layout

---
+++ app/src/main/java/com/mad/p03/np2020/routine/Class/FocusAdapter.java
@@ -0,0 +1,27 @@
+package com.mad.p03.np2020.routine;
+
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class FocusAdapter extends RecyclerView.Adapter<FocusAdapter> {
+
+    private List<Focus> focusList;
+
+    @NonNull
+    @Override
+    public FocusAdapter onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull FocusAdapter holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
