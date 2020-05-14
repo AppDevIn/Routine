df49a7c76a99866cd9f574b5f1c3048d0d2d8c94
Pritheev-Roshan <s10193030@connect.np.edu.sg>
2020-05-14 00:55:36 -0700

    Card Layout 0.1

---
+++ app/src/main/java/com/mad/p03/np2020/routine/CardActivity.java
@@ -0,0 +1,18 @@
+package com.mad.p03.np2020.routine;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Paint;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CardActivity extends AppCompatActivity {
+
+}
