38491e0961a0c638cc2d20768b953026a0ad7071
“AppDevIn” <teamprojectlive@gmail.com>
2020-05-17 01:01:26 +0800

    Card view for the register

--- app/src/main/java/com/mad/p03/np2020/routine/Register.java
+++ app/src/main/java/com/mad/p03/np2020/routine/Register.java
@@ -1,8 +1,15 @@
 package com.mad.p03.np2020.routine;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 
 public class Register extends AppCompatActivity {
 
@@ -11,4 +18,23 @@ public class Register extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+//
+//        SignUpPop signUpPop = new SignUpPop();
+//        signUpPop.show(getSupportFragmentManager(),"Sign up dialog");
+
+
+
+    }
+
+
 }
+
