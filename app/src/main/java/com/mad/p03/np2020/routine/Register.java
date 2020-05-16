2ff5ea7e80293962e91aa7b731f1ed9b48370e72
“AppDevIn” <teamprojectlive@gmail.com>
2020-05-17 02:23:17 +0800

    Layout for updating card view

--- app/src/main/java/com/mad/p03/np2020/routine/Register.java
+++ app/src/main/java/com/mad/p03/np2020/routine/Register.java
@@ -1,40 +1,124 @@
 package com.mad.p03.np2020.routine;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.DialogFragment;
 
-import android.app.AlertDialog;
-import android.app.Dialog;
-import android.content.DialogInterface;
 import android.os.Bundle;
-import android.view.LayoutInflater;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class Register extends AppCompatActivity {
+    
+    //Declare Constants
+    final static String TAG = "Register";
+
+    //Declare member variables
+    Button mSubmit;
+    int progessCount = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        Log.d(TAG, "onCreate: Doing binding and startup logic");
     }
 
     @Override
     protected void onStart() {
         super.onStart();
+        Log.d(TAG, "onStart: GUI is started ready to move to foreground");
+
+        //Find IDs
+        mSubmit = findViewById(R.id.btnSubmit);
+
+
+
     }
+
+
 
     @Override
     protected void onResume() {
         super.onResume();
-//
-//        SignUpPop signUpPop = new SignUpPop();
-//        signUpPop.show(getSupportFragmentManager(),"Sign up dialog");
+        Log.d(TAG, "onResume: GUI is in the Foreground");
 
 
+        //Listener to the submit button
+        mSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                updateCardView(true);
+            }
+        });
 
     }
 
+    private void updateCardView(Boolean isForward){
+        if (isForward) {
+            progessCount++;
+        } else {
+            progessCount--;
+        }
+
+        switch (progessCount){
+            case 1:
+                askName();
+                break;
+            case 2:
+                askEmail();
+                break;
+            case 3:
+                askPassword();
+                break;
+            case 4:
+                askConfirmPassword();
+                break;
+            case 5:
+                askDateOfBirth();
+                break;
+            case 6:
+                startRegistration();
+                break;
+            default: 
+                askName();
+                break;
+        }
+    }
+
+    //Changes the cardview details to name
+    private void askName() {
+        Log.d(TAG, "askName: ");
+
+    }
+
+    //Changes the cardview details to emial
+    private void askEmail() {
+        Log.d(TAG, "askEmail: ");
+
+    }
+
+    //Changes the cardview details to Password
+    private void askPassword() {
+        Log.d(TAG, "askPassword: ");
+
+    }
+
+    //Changes the cardview details to Confirm Password
+    private void askConfirmPassword() {
+        Log.d(TAG, "askConfirmPassword: ");
+
+    }
+
+    //Changes the cardview details to Confirm Password
+    private void askDateOfBirth() {
+        Log.d(TAG, "askDateOfBirth: ");
+
+    }
+    
+    private void startRegistration(){
+        Log.d(TAG, "startRegistration: ");
+    }
 
 }
+
 
