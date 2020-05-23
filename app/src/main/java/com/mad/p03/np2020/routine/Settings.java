37ddecc2846fe50c0f45968294ab96e3250c292a
Pritheev-Roshan <s10193030@connect.np.edu.sg>
2020-05-23 01:23:00 -0700

    Added Profile Picture spot and profile picture button

---
+++ app/src/main/java/com/mad/p03/np2020/routine/Settings.java
@@ -0,0 +1,14 @@
+package com.mad.p03.np2020.routine;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Settings extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
