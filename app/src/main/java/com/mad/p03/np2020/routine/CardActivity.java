8e62cc8c367f807be21ce8144dc15bbccf7c93d1
Pritheev-Roshan <s10193030@connect.np.edu.sg>
2020-05-23 01:14:37 -0700

    Modified CardAcivity

--- app/src/main/java/com/mad/p03/np2020/routine/CardActivity.java
+++ app/src/main/java/com/mad/p03/np2020/routine/CardActivity.java
@@ -8,18 +8,31 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
+
+import com.mad.p03.np2020.routine.Class.Steps;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class CardActivity extends AppCompatActivity {
+
+    final String TAG = "Card Layout";
+    ArrayList<String> data = new ArrayList<>();
+    ArrayList<Steps> StepItem = new ArrayList<>();
+    RecyclerView cardRecycler;
+    Button stepAddButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        RecyclerView cardRecycler = findViewById(R.id.cardRecyclerView);
+        cardRecycler = findViewById(R.id.cardRecyclerView);
+        stepAddButton = findViewById(R.id.stepAddButton);
+
     }
 }
