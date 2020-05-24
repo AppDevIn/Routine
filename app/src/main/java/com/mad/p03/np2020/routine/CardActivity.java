d8e6e297715f45002fe7ed31cb16fa2702e827a3
Pritheev-Roshan <s10193030@connect.np.edu.sg>
2020-05-23 22:07:20 -0700

    Modified CardLayout

--- app/src/main/java/com/mad/p03/np2020/routine/CardActivity.java
+++ app/src/main/java/com/mad/p03/np2020/routine/CardActivity.java
@@ -29,7 +29,7 @@ public class CardActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.card_layout);
 
         cardRecycler = findViewById(R.id.cardRecyclerView);
         stepAddButton = findViewById(R.id.stepAddButton);
