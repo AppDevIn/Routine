a00bc5cd86de2356119072418b296769040e75e1
AppDevIn <teamprojectlive@gmail.com>
2020-05-31 01:39:42 +0800

    Done with task design

---
+++ app/src/main/java/com/mad/p03/np2020/routine/task.java
@@ -0,0 +1,14 @@
+package com.mad.p03.np2020.routine;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class task extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_task);
+    }
+}
