a29731f40a5ab1cba859e81f51e96cf8b5e64474
iamgruuten <52743433+iamgruuten@users.noreply.github.com>
2020-05-31 21:38:51 +0800

    Update home

--- app/src/main/java/com/mad/p03/np2020/routine/Home.java
+++ app/src/main/java/com/mad/p03/np2020/routine/Home.java
@@ -1,94 +1,198 @@
 package com.mad.p03.np2020.routine;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 import androidx.core.content.ContextCompat;
 import androidx.core.graphics.drawable.DrawableCompat;
+import androidx.lifecycle.LifecycleOwner;
+import androidx.lifecycle.Observer;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.ItemTouchHelper;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.work.Constraints;
+import androidx.work.Data;
+import androidx.work.NetworkType;
+import androidx.work.OneTimeWorkRequest;
+import androidx.work.WorkInfo;
+import androidx.work.WorkManager;
+import androidx.work.Worker;
 
+import android.annotation.SuppressLint;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.Html;
 import android.util.Log;
 import android.view.KeyEvent;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.WindowManager;
 import android.view.inputmethod.EditorInfo;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.GridView;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.mad.p03.np2020.routine.Adapter.HomePageAdapter;
+import com.mad.p03.np2020.routine.Adapter.ItemTouchHelperAdapter;
+import com.mad.p03.np2020.routine.Adapter.MyItemTouchHelper;
+import com.mad.p03.np2020.routine.Adapter.MySpinnerApater;
+import com.mad.p03.np2020.routine.Adapter.MySpinnerBackgroundAdapter;
+import com.mad.p03.np2020.routine.Adapter.OnSectionListener;
 import com.mad.p03.np2020.routine.Class.Section;
-import com.mad.p03.np2020.routine.Class.Task;
+import com.mad.p03.np2020.routine.Class.User;
+import com.mad.p03.np2020.routine.background.UploadDataWorker;
+import com.mad.p03.np2020.routine.background.UploadSectionWorker;
+import com.mad.p03.np2020.routine.database.MyDatabaseListener;
+import com.mad.p03.np2020.routine.database.SectionDBHelper;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class Home extends AppCompatActivity {
+public class Home extends AppCompatActivity implements MyDatabaseListener {
 
     //Declare Constants
     final String TAG = "Home Activity";
 
 
     //Declare member variables
-    GridView mGridView;
+    RecyclerView mGridView;
     HomePageAdapter mHomePageAdapter;
-    ImageButton mImgAdd;
+
     EditText mEditAddList;
+    Spinner mSpinnerColor, mSpinnerBackground;
+    CardView mCardViewPopUp;
+    Button mBtnAdd, mBtnCancel;
+    FloatingActionButton mImgAdd;
+    List<Section> mSectionList;
+    SectionDBHelper mSectionDBHelper;
+    Integer[] mColors, mBackgrounds;
+    String mUID;
+    User mUser;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        Log.d(TAG, "UI is being created");
 
-        //TODO: Find view by ID
+        //TODO: Get user from the intent
+        mUser = getIntent().getParcelableExtra("user");
+//        mUID = mUser.getUID();
+
+        mUID = "aRbjnh0WpNe8gGga1PkDfOiJLH03";
+
+
+
+        //Find view by ID
         mGridView = findViewById(R.id.section_grid_view);
         mImgAdd = findViewById(R.id.imgBtnTodo);
         mEditAddList = findViewById(R.id.txtAddList);
+        mSpinnerColor = findViewById(R.id.spinnerColor);
+        mSpinnerBackground = findViewById(R.id.spinnerImg);
+        mCardViewPopUp = findViewById(R.id.cardViewPopUp);
+        mBtnAdd = findViewById(R.id.btnAdd);
+        mBtnCancel = findViewById(R.id.btnCancel);
+
+
+
+        //Get all the section data from firebase
+        mSectionDBHelper = new SectionDBHelper(this);
+        mSectionList = mSectionDBHelper.getAllSections(mUID);
+        Log.d(TAG, "onCreate(): Data received from SQL");
+
+        //A list of possible colors to be user
+        mColors = new Integer[]{getResources().getColor(R.color.superiorityBlue), getResources().getColor(R.color.rosyBrown), getResources().getColor(R.color.mandarin), getResources().getColor(R.color.green_yellow), getResources().getColor(R.color.turquoise)};
+
+        //A list of possible background to be user
+        mBackgrounds = new Integer[] {R.drawable.amazon, R.drawable.android, R.drawable.laptop, R.drawable.code, R.drawable.bookmark};
+
+
+        //To set to Full screen
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
     }
 
     @Override
     protected void onStart() {
         super.onStart();
+        Log.d(TAG, "onStart: GUI is ready");
 
-        //TODO: Initialize any value
-        mHomePageAdapter = new HomePageAdapter(this,R.layout.home_grid_view_items, hardCodedList());
+
+        //Recycler view setup
+        mGridView.setLayoutManager(new GridLayoutManager(Home.this,2)); //Setting the layout manager with the column of 2
+        mGridView.addItemDecoration(new DividerItemDecoration(25));
+
+
+        // Initialize any value
+        mHomePageAdapter = new HomePageAdapter(mSectionList, this);
         mGridView.setAdapter(mHomePageAdapter);
+
+        //Declaring a custom adapter
+        mSpinnerColor.setAdapter(new MySpinnerApater( mColors)); // For the color
+        mSpinnerBackground.setAdapter(new MySpinnerBackgroundAdapter(mBackgrounds)); //For the background
 
         //Set the cursor to the start
         mEditAddList.setSelection(0);
 
+        //Setting up touchhelper
+        ItemTouchHelper.Callback callback = new MyItemTouchHelper(mHomePageAdapter);
+        ItemTouchHelper itemTouchHelper = new ItemTouchHelper(callback);
+        mHomePageAdapter.setTouchHelper(itemTouchHelper);
+        itemTouchHelper.attachToRecyclerView(mGridView);
 
 
+        //Make sure the card view is not visible
+        mCardViewPopUp.setVisibility(View.INVISIBLE);
+        Log.d(TAG, "onStart(): Card view is set to invisible");
+
+
+        mImgAdd.setImageResource(R.drawable.ic_add_black_24dp);
     }
 
     @Override
     protected void onResume() {
         super.onResume();
+        Log.d(TAG, "onResume: GUI is in the Foreground and Interactive");
 
-        mGridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                //Logging the name of the list clicked
-                Log.i("Home Activity", "onItemClick: "+hardCodedList().get(i).getName());
-            }
-        });
-        
+        //When the add button is clicked make the card view visible
         mImgAdd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Log.i(TAG, "onClick: Adding new list clicked");
+                Log.i(TAG, "onClick(): Adding new list clicked");
 
-                //Make the edit text visible for the use to type
-                mEditAddList.setVisibility(View.VISIBLE);
-
+                //Make the card view visible to the user
+                mCardViewPopUp.setVisibility(View.VISIBLE);
+                Log.d(TAG, "onClick(): Cardview is visible");
 
             }
         });
 
+        //Checks for the enter action from the keyboard
         mEditAddList.setOnEditorActionListener(new TextView.OnEditorActionListener() {
             @Override
             public boolean onEditorAction(TextView textView, int actionId, KeyEvent event) {
@@ -97,68 +201,127 @@ public class Home extends AppCompatActivity {
                         event.getAction() == KeyEvent.ACTION_DOWN &&
                                 event.getKeyCode() == KeyEvent.KEYCODE_ENTER) {
 
-                    String listName = textView.getText().toString();
-                    Log.i(TAG, "onEditorAction: " + listName);
+                    Log.d(TAG, "onEditorAction(): User eneted \"ENTER\" in keyboard ");
+                    addSection(textView);
 
-                    addSection(listName);
-
-                    //The Textview disappear after entered is pressed
-                    mEditAddList.setVisibility(View.INVISIBLE);
-
-                    //Empty the edit text
-                    mEditAddList.setText("");
-
-                    //Hide the soft keyboard
-                    InputMethodManager mgr = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
-                    assert mgr != null;
-                    mgr.hideSoftInputFromWindow(mEditAddList.getWindowToken(), 0);
-
-                    return true; // consume.
+                    return true;
                 }
 
                 return false;
             }
         });
 
+        //When the add button is clicked
+        mBtnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d(TAG, "onClick(): Add button is pressed ");
+                addSection(mEditAddList);
+            }
+        });
+
+
+        //If the cancel button is pressed
+        mBtnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                updateCard();
+            }
+        });
+
+        SectionDBHelper.setMyDatabaseListener(this);
 
     }
 
 
-    void addSection(String name){
 
-        Section section = new Section(name, new ArrayList<Task>(),"#CAF4F4");
+    /**
+     *
+     * Triggered to add to the current adapter list
+     * when it is added to the sql
+     *
+     * @param section given from the SQL when triggered
+     */
+    @Override
+    public void onSectionAdd(final Section section) {
+        Log.d(TAG, "A new section has been added: " + section.toString());
 
-        //Add new section object into the adapter
-        mHomePageAdapter.add(section);
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                //Add to List<Section>
+                mHomePageAdapter.addItem(section);
+            }
+        });
+    }
 
-        //TODO: Add the object the user class
+    @Override
+    public void onSectionDelete(String ID) {
+        //Remove item from the data in adapter/local list
+
+
+        for (int position = 0; position < mSectionList.size(); position++) {
+
+
+            if(mSectionList.get(position).getID().equals(ID)){
+                final int finalPosition = position;
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        mHomePageAdapter.removeItem(finalPosition);
+                    }
+                });
+                break;
+            }
+        }
+
+    }
+
+    /**
+     *
+     * To add the data into firebase and SQL from the card
+     * Create a section object.
+     *
+     * @param textView the is the edittext
+     */
+    private void addSection(TextView textView){
+        String listName = textView.getText().toString();
+        Log.i(TAG, "adding confirmed for " + listName);
+
+        //Create a Section Object for the user input
+        Section section = new Section(textView.getText().toString().trim(), mColors[mSpinnerColor.getSelectedItemPosition()], mBackgrounds[mSpinnerBackground.getSelectedItemPosition()]);
+
+        //Save to SQL
+        String id  = section.addSection(this, mUID);
+        Log.d(TAG, "updateCardUI(): Added to SQL, this ID is " + id);
+
+        //Save to firebase
+        section.executeFirebaseSectionUpload(mUID, section.getID(), this);
+
+        //Function that make the cardview invisible and hide keyboard
+        updateCard();
+    }
+
+
+
+    /**
+     *  Set the cardview invisible and hide the keyboard
+     *
+     */
+    void updateCard(){
+        //The card view will disappear
+        mCardViewPopUp.setVisibility(View.INVISIBLE);
+        Log.d(TAG, "updateCardUI(): Card view is invisible");
+
+        //Hide the soft keyboard
+        InputMethodManager mgr = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        assert mgr != null;
+        mgr.hideSoftInputFromWindow(mEditAddList.getWindowToken(), 0);
     }
 
 
 
 
-    //Temp hard code for fake user data
-    List<Section> hardCodedList(){
-        List<Section> sectionList = new ArrayList<>();
-        List<Task> taskList = new ArrayList<>();
-
-        //TODO: Add in fake data
-        taskList.add(new Task("Kill myself"));
-        taskList.add(new Task("Kill myself"));
-        taskList.add(new Task("Kill myself"));
-        taskList.add(new Task("Kill myself"));
-        taskList.add(new Task("Kill myself"));
-        taskList.add(new Task("Kill myself"));
 
 
-        sectionList.add(new Section("MAD", taskList,"#CAF4F4"));
-        sectionList.add(new Section("Home", taskList,"#B8EFEF"));
-        sectionList.add(new Section("Web", taskList, "#FFEAAC"));
-        sectionList.add(new Section("School", taskList, "#FFC2B4"));
-
-
-        return sectionList;
-
-    }
 }
-
