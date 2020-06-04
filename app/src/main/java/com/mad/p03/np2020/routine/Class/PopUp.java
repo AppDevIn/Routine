6f43998e4ce8cf8198550d06df14e72051d5fba5
Pritheev-Roshan <s10193030@connect.np.edu.sg>
2020-06-04 07:34:45 -0700

    Added timer functionality

--- app/src/main/java/com/mad/p03/np2020/routine/Class/PopUp.java
+++ app/src/main/java/com/mad/p03/np2020/routine/Class/PopUp.java
@@ -3,6 +3,7 @@ package com.mad.p03.np2020.routine.Class;
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
@@ -17,7 +18,9 @@ public class PopUp extends Activity {
 
     TextView TimerLeft;
     TextView TimerRight;
-    TextView TimerColon;
+
+    public int hours = 0;
+    public int minutes = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -30,6 +33,41 @@ public class PopUp extends Activity {
         DownArrowLeft = findViewById(R.id.LeftDown);
         DownArrowRight = findViewById(R.id.RightDown);
 
+        TimerLeft = findViewById(R.id.timerLeft);
+        TimerRight = findViewById(R.id.timerRight);
+
+        UpArrowLeft.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                hours += 1;
+                TimerLeft.setText(timeToText(hours, 24));
+            }
+        });
+
+        DownArrowLeft.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                hours -= 1;
+                TimerLeft.setText(timeToText(hours, 24));
+            }
+        });
+
+        UpArrowRight.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                minutes += 1;
+                TimerRight.setText(timeToText(minutes, 60));
+            }
+        });
+
+        DownArrowRight.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                minutes -= 1;
+                TimerRight.setText(timeToText(minutes, 60));
+            }
+        });
+
         DisplayMetrics dm = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(dm);
 
@@ -38,6 +76,30 @@ public class PopUp extends Activity {
 
         getWindow().setLayout((int) (width*.6), (int) (height*.4));
 
+    }
+
+    public String timeToText(int time, int limit)
+    {
+        String timer = "";
+        if (time < 10 && time >= 0)
+        {
+            timer = "0" + String.valueOf(time);
+            return timer;
+        }
+        else if (time < 0)
+        {
+            timer = "00";
+            return timer;
+        }
+
+        if (time >= limit)
+        {
+            timer = String.valueOf(limit-1);
+            return timer;
+        }
+
+        timer = String.valueOf(time);
+        return timer;
     }
 
 }
