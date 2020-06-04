e6c606cbbb5e3b67dc9be64cbca63136d5ab7d6e
Pritheev-Roshan <s10193030@connect.np.edu.sg>
2020-06-04 06:58:44 -0700

    Added notification timer layout

--- app/src/main/java/com/mad/p03/np2020/routine/Class/PopUp.java
+++ app/src/main/java/com/mad/p03/np2020/routine/Class/PopUp.java
@@ -3,10 +3,21 @@ package com.mad.p03.np2020.routine.Class;
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import com.mad.p03.np2020.routine.R;
 
 public class PopUp extends Activity {
+
+    ImageButton UpArrowLeft;
+    ImageButton UpArrowRight;
+    ImageButton DownArrowLeft;
+    ImageButton DownArrowRight;
+
+    TextView TimerLeft;
+    TextView TimerRight;
+    TextView TimerColon;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -14,11 +25,18 @@ public class PopUp extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.popupwindow);
 
+        UpArrowLeft = findViewById(R.id.LeftTop);
+        UpArrowRight = findViewById(R.id.RightTop);
+        DownArrowLeft = findViewById(R.id.LeftDown);
+        DownArrowRight = findViewById(R.id.RightDown);
+
         DisplayMetrics dm = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(dm);
 
         int height = dm.heightPixels;
         int width = dm.widthPixels;
+
+        getWindow().setLayout((int) (width*.8), (int) (height*.6));
 
     }
 
