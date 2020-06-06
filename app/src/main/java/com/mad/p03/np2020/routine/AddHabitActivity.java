a8d054604521603bfc1ec8ebbe761b7992d0a417
hwen <whman63@gmail.com>
2020-06-06 12:28:15 +0800

    added add habit activity

---
+++ app/src/main/java/com/mad/p03/np2020/routine/AddHabitActivity.java
@@ -0,0 +1,17 @@
+package com.mad.p03.np2020.routine;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AddHabitActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.add_habit);
+
+
+    }
+}
