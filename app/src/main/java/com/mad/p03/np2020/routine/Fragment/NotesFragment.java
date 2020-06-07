562768f812286cab2ede4979f9cd9644871026b9
AppDevIn <teamprojectlive@gmail.com>
2020-06-07 19:41:51 +0800

    Save notes to object when enter key is pressed

--- app/src/main/java/com/mad/p03/np2020/routine/Fragment/NotesFragment.java
+++ app/src/main/java/com/mad/p03/np2020/routine/Fragment/NotesFragment.java
@@ -4,11 +4,17 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
 
+import com.mad.p03.np2020.routine.Class.Task;
 import com.mad.p03.np2020.routine.R;
+import com.mad.p03.np2020.routine.database.TaskDBHelper;
 
 /*
  *
@@ -18,20 +24,20 @@ import com.mad.p03.np2020.routine.R;
  * @since 02-06-2020
  *
  */
-
 public class NotesFragment extends Fragment
 {
 
+    Task mTask;
     //Empty constructor
-    public NotesFragment() {
-        // Required empty public constructor
+    public NotesFragment(Task task) {
+        mTask = task;
     }
 
     //Initializing variables for NotesFragment when new instance created
-    public static NotesFragment newInstance()
+    public static NotesFragment newInstance(Task task)
     {
         //Initializing a new instance of NotesFragment
-        NotesFragment fragment = new NotesFragment();
+        NotesFragment fragment = new NotesFragment(task);
 
         //Initializing Bundle
         Bundle args = new Bundle();
@@ -53,7 +59,28 @@ public class NotesFragment extends Fragment
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        final View view = inflater.inflate(R.layout.fragment_notes_layout, container, false);
+
+        EditText notes = view.findViewById(R.id.notes);
+
+        notes.setText(mTask.getNotes());
+
+        notes.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) {
+
+                Log.d("NotesFragment", "onEditorAction: " + textView.getText());
+                mTask.setNotes(textView.getText().toString());
+
+                TaskDBHelper taskDBHelper = new TaskDBHelper(view.getContext());
+                taskDBHelper.update(mTask.getTaskID(),null, mTask.getNotes());
+
+                return false;
+            }
+        });
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_notes_layout, container, false);
+        return view;
     }
 }
