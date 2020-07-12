cc9932332db2a32b18e8f91e47d7b19cc6599708
Pritheev-Roshan <s10193030@connect.np.edu.sg>
2020-07-12 16:18:02 -0700

    Modified notification in CardNotification

--- app/src/main/java/com/mad/p03/np2020/routine/models/CardNotification.java
+++ app/src/main/java/com/mad/p03/np2020/routine/models/CardNotification.java
@@ -1,8 +1,14 @@
 package com.mad.p03.np2020.routine.models;
 
+import android.app.AlarmManager;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
 
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
@@ -10,18 +16,27 @@ import androidx.core.app.NotificationManagerCompat;
 import com.mad.p03.np2020.routine.R;
 
 public class CardNotification extends BroadcastReceiver {
+
+    private String channelID = "CardNotification";
+
     @Override
     public void onReceive(Context context, Intent intent)
     {
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, "notifyCard")
-                .setSmallIcon(R.drawable.logo)
-                .setContentTitle("Test")
-                .setContentText("This is a test")
-                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+        if (intent.getAction().equals("CardNotification"))
+        {
+            Bundle bundle = intent.getExtras();
 
-        NotificationManagerCompat notificationManagerCompat = NotificationManagerCompat.from(context);
+            NotificationCompat.Builder notification = new NotificationCompat.Builder(context, channelID)
+                    .setSmallIcon(R.drawable.logo)
+                    .setContentTitle("Routine")
+                    .setContentText("Your card is due!")
+                    .setPriority(NotificationCompat.PRIORITY_DEFAULT);
 
-        notificationManagerCompat.notify(200,  builder.build());
+            NotificationManagerCompat notificationManagerCompat = NotificationManagerCompat.from(context);
+
+            notificationManagerCompat.notify(200, notification.build());
+        }
+
 
     }
 }
